f5ace7ee5e5bc7201f890c37dd350eafc1a00705
Jose Briones <josmbrio@gmail.com>
2022-05-19 02:06:35 -0500

    initial commit

---
+++ ex6.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+NEW_USER=myapp
+
+echo
+echo === Instalando Nodejs y Npm
+sleep 1
+apt-get -y install nodejs npm 
+
+echo 
+echo === Creando usuario myapp
+useradd -m $NEW_USER
+
+echo
+echo === Descargando bootcamp-node-envvars-project
+sleep 1
+runuser -l $NEW_USER -c "wget https://node-envvars-artifact.s3.eu-west-2.amazonaws.com/bootcamp-node-envvars-project-1.0.0.tgz"
+
+echo
+echo === Untar y Unzip de paquete
+sleep 1
+runuser -l $NEW_USER -c "tar xvzf bootcamp-node-envvars-project-1.0.0.tgz"
+
+echo
+echo === Listando el contenido descargado
+sleep 1
+cd package
+echo
+ls -l
+echo
+
+echo
+read -p "=== Ingrese el directorio de log (ruta absoluta): " log_directory
+
+if ! [ -d "$log_directory" ]
+then
+  mkdir -p "$log_directory"
+fi
+
+LOG_DIR="$log_directory"
+export LOG_DIR
+echo
+echo === Nuevo LOG_DIR: $LOG_DIR
+
+chown $NEW_USER:$NEW_USER $LOG_DIR
+
+sleep 1
+PID=$(pidof node)
+if ! [ -z $PID ]
+then
+	echo
+	echo "=== El servicio ya esta levantado con el PID $PID y en el puerto $(netstat -anp| grep node| awk '{print $4}'|awk -F':' '{print $NF}')"
+else
+  echo
+  echo === Ejecutando \'npm install\' y \'node server.js\'
+  echo "=== Directorio actual: $(pwd)"
+  sleep 3
+  runuser -l $NEW_USER -c "
+  export APP_ENV=dev &&
+  export DB_USER=myuser &&
+  export DB_PWD=mypassword &&
+  export LOG_DIR=$log_directory &&
+  cd package &&
+  npm install &&
+  node server.js &"
+  sleep 10
+  PID=$(pidof node)
+  echo
+  echo "=== El servicio está levantado con el PID $PID y en el puerto $(netstat -anp| grep node| awk '{print $4}'|awk -F':' '{print $NF}')"
+fi
+
